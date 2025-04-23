5992cae1ced4ad94d16e1e647d55f45d7c271ebe
Glauco Vasconcelos <113292955+Andrade-14@users.noreply.github.com>
2025-04-23 14:10:22 -0300

    Delete src directory

--- src/controllers/healthController.js
+++
@@ -1,7 +0,0 @@
-// Função para checar o status da API
-exports.checkHealth = (req, res) => {
-  res.status(200).json({
-    message: 'API está funcionando corretamente!',
-    timestamp: new Date(),
-  });
-};
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const healthController = require('../controllers/healthController');
-
-// Rota de saúde (health check)
-router.get('/health', healthController.checkHealth);
-
-module.exports = router;
